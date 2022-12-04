bf92b0d00039a4fc611fe50a083f8a4313992298
Resxt <55228336+Resxt@users.noreply.github.com>
2022-12-04 19:24:23 +0100

    artist-discography-to-playlist 0.0.2
    When right clicking on an artist and clicking on "Create a playlist from discography":
    Creates an empty playlist with the default artist image, a default description and default settings (private playlist etc.)

--- artist-discography-to-playlist/src/types/artist-discography-to-playlist.d.ts
+++ artist-discography-to-playlist/src/types/artist-discography-to-playlist.d.ts
@@ -1,4 +1,13 @@
 type ArtistData = {
+    name: string,
+    images: ArtistImageData[]
+}
+
+type ArtistImageData = {
+    url: string
+}
+
+type AlbumsData = {
     items: [],
     next: string,
     total: number
@@ -6,4 +15,20 @@ type ArtistData = {
 
 type SongData = {
     album_type: string,
+}
+
+type PlaylistData = {
+    name: string,
+    id: string
+}
+
+type UserData = {
+    id: string
+}
+
+type CreatePlaylistBody = {
+    name: string,
+    public: boolean,
+    collaborative: boolean,
+    description: string
 }
