28394b2e5c7e872610f73090acacdbae91c31290
Resxt <55228336+Resxt@users.noreply.github.com>
2026-04-10 14:54:18 +0200

    image-opener 1.0.0

---
+++ image-opener/README.md
@@ -0,0 +1,31 @@
+# image-opener
+
+![preview](https://raw.githubusercontent.com/Resxt/Spicetify-Extensions/main/image-opener/assets/preview.png)
+
+`image-opener` is a [Spicetify](https://spicetify.app/) extension that allows you to open various images directly in your browser by right clicking the associated element.  
+
+Supported images are:
+
+- Artist avatar
+- Artist banner
+- Artist gallery
+- Track cover
+- Album cover
+- Playlist cover
+
+## How to use it?
+
+For single picture elements you can right click an associated element (track name, album cover etc.) then click on the "Open cover" button.  
+
+Elements that may contain more than one picture such as artists use an "Open image" sub menu instead.  
+Right click an associated element (artist name, artist banner etc.) then simply click on the button you want within that sub menu (includes a button to open all of them in one click!)
+
+## Credits
+
+- This extension is based on the archived [Image-Opener extension by Tetrax10](https://github.com/Tetrax-10/Spicetify-Extensions/tree/master/Image-Opener)
+
+- Thanks to `XJ-090` for providing a way to get a track's cover image
+
+## Made with Spicetify Creator
+
+- <https://github.com/spicetify/spicetify-creator>
---
+++ image-opener/src/app.tsx
@@ -0,0 +1,198 @@
+async function main() {
+  while (!Spicetify?.showNotification) {
+    await new Promise(resolve => setTimeout(resolve, 100));
+  }
+
+  const artistAvatarImageMenuItem = new Spicetify.ContextMenu.Item("Artist avatar image", onArtistAvatarImageMenuItemClicked, () => { return true }, "external-link");
+  const artistBannerImageMenuItem = new Spicetify.ContextMenu.Item("Artist banner image", onArtistBannerImageMenuItemClicked, () => { return true }, "external-link");
+  const artistGalleryImagesMenuItem = new Spicetify.ContextMenu.Item("Artist gallery images", onArtistGalleryImagesMenuItemClicked, () => { return true }, "external-link");
+  const artistAllImagesMenuItem = new Spicetify.ContextMenu.Item("All artist images", onArtistAllImagesMenuItemClicked, () => { return true }, "external-link");
+
+  new Spicetify.ContextMenu.SubMenu("Open image", [artistAvatarImageMenuItem, artistBannerImageMenuItem, artistGalleryImagesMenuItem, artistAllImagesMenuItem], (uris) => {
+    return Spicetify.URI.isArtist(uris[0]);
+  }).register();
+
+  new Spicetify.ContextMenu.Item("Open cover image", onAlbumMenuItemClicked, (uris) => {
+    const uri = uris[0];
+
+    return (Spicetify.URI.isTrack(uri) || Spicetify.URI.isAlbum(uri));
+  },
+    "external-link").register();
+
+  new Spicetify.ContextMenu.Item("Open cover image", onPlaylistMenuItemClicked, (uris) => {
+    const uri = uris[0];
+
+    return Spicetify.URI.isPlaylistV1OrV2(uri);
+  },
+    "external-link").register();
+}
+
+
+
+let lastFetchedArtistUri: string;
+let lastFetchedArtistData: any;
+
+
+
+function onArtistAvatarImageMenuItemClicked(uris: string[]) {
+  openArtistAvatarImage(uris[0]);
+}
+
+function onArtistBannerImageMenuItemClicked(uris: string[]) {
+  openArtistBannerImage(uris[0]);
+}
+
+function onArtistGalleryImagesMenuItemClicked(uris: string[]) {
+  openArtistGalleryImages(uris[0]);
+}
+
+async function onArtistAllImagesMenuItemClicked(uris: string[]) {
+  onArtistAvatarImageMenuItemClicked(uris);
+  await new Promise(resolve => setTimeout(resolve, 500));
+  onArtistBannerImageMenuItemClicked(uris);
+  await new Promise(resolve => setTimeout(resolve, 500));
+  onArtistGalleryImagesMenuItemClicked(uris);
+}
+
+function onAlbumMenuItemClicked(uris: string[]) {
+  openAlbumCoverImage(uris[0]);
+}
+
+function onPlaylistMenuItemClicked(uris: string[]) {
+  openPlaylistCoverImage(uris[0]);
+}
+
+
+
+async function openArtistAvatarImage(uri: string) {
+  const artistVisuals = await getArtistVisuals(uri);
+
+  window.open(artistVisuals.avatarImage.sources[0].url);
+}
+
+async function openArtistBannerImage(uri: string) {
+  const artistVisuals = await getArtistVisuals(uri);
+
+  if (artistVisuals.headerImage == null) {
+    throw logError("Artist does not have a banner image");
+  }
+  else {
+    window.open(artistVisuals.headerImage.sources[0].url);
+  }
+}
+
+async function openArtistGalleryImages(uri: string) {
+  const artistVisuals = await getArtistVisuals(uri);
+
+  if (artistVisuals.gallery.items.length == 0) {
+    throw logError("Artist does not have any images in gallery");
+  }
+  else {
+    artistVisuals.gallery.items.forEach((element: { sources: { url: any; }[]; }) => {
+      window.open(element.sources[0].url);
+    });
+  }
+}
+
+async function openAlbumCoverImage(uri: string) {
+  if (Spicetify.URI.isAlbum(uri)) {
+    const albumData = await getAlbumData(uri);
+
+    window.open(albumData.data.albumUnion.coverArt.sources[2].url);
+  }
+  else {
+    const trackData = await getTrackData(uri);
+
+    window.open(trackData.data.tracks[0].albumOfTrack.coverArt.sources[2].url);
+  }
+}
+
+async function openPlaylistCoverImage(uri: string) {
+  const playlistData = await getPlaylistData(uri);
+
+  window.open(playlistData.data.playlistV2.images.items[0].sources[0].url);
+}
+
+
+
+/**
+ * This function stores the fetched artist's data in a variable to avoid making additional (unnecessary) API calls  
+ * when the same artist's data is being requested several times in a row
+ * 
+ * For example, this behavior is observed by requesting the artist's data 3 times in just one function call  
+ * with the `onArtistAllImagesMenuItemClicked` function
+ */
+async function getArtistData(uri: string) {
+  try {
+    if (lastFetchedArtistUri != uri) {
+      lastFetchedArtistUri = uri;
+
+      lastFetchedArtistData = getData("queryArtistOverview",
+        {
+          uri: uri,
+          includePrerelease: false
+        })
+    }
+
+    return lastFetchedArtistData;
+  } catch (error) {
+    throw logError("Failed to retrieve artist data");
+  }
+}
+
+async function getArtistVisuals(uri: string) {
+  const artistData = await getArtistData(uri);
+
+  return artistData.data.artistUnion.visuals;
+}
+
+async function getTrackData(uri: string) {
+  return getData("decorateContextTracks",
+    { uris: [uri] });
+}
+
+async function getAlbumData(uri: string) {
+  return getData("getAlbum", {
+    uri: uri,
+    offset: 0,
+    limit: 1
+  });
+}
+
+async function getPlaylistData(uri: string) {
+  return getData("fetchPlaylist", {
+    uri: uri
+  });
+}
+
+
+
+async function getData(query: any, variables: any) {
+  try {
+    return await fetchAndValidateGraphQL(
+      Spicetify.GraphQL.Definitions[query],
+      variables
+    );
+  } catch (error) {
+    throw logError("Failed to retrieve data");
+  }
+}
+
+async function fetchAndValidateGraphQL(query: any, variables: any) {
+  const response = await Spicetify.GraphQL.Request(query, variables);
+
+  if (response.errors && response.errors.length > 0) {
+    throw new Error("Data is undefined or empty");
+  }
+
+  return response;
+}
+
+function logError(message: string) {
+  Spicetify.showNotification(message, true, 2500);
+  throw new Error(message);
+}
+
+
+
+export default main;
